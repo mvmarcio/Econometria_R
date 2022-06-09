969add348022fc9396d7be20fbf0ce7c2d8a5a0e
Márcio Alves <32937946+mvmarcio@users.noreply.github.com>
2022-06-08 21:16:36 -0300

    Update script_enade_2018_10_10_21.R

--- script_enade_2018_10_10_21.R
+++ script_enade_2018_10_10_21.R
@@ -1,29 +1,29 @@
 # Script para o trabalho de Econometria
 
 # 1. Importando pacotes necessarios:
-library(car) #pacote para testes do modelo de regressao
-library(AER) #pacote para mais testes do modelos
-library(psych) #pacote para estatisticas descritivas
-library(dplyr) #pacote para organizacao e tratamento dos dados
-library(tidyr) #pacote para organizacao e tratamento dos dados
-library(DescTools) #pacote para correcao de outliers
-library(normtest) #pacote para testes de normalidade
-library(openxlsx) #leitura dos dados em excel
-library(readxl) #leitura dos dados em Excel
-library(stargazer) #tabelas das regressoes
-library(Hmisc) #correlacoes e testes
-library(tidyverse) #organizar os dados
+library(car) # pacote para testes do modelo de regressao
+library(AER) # pacote para mais testes do modelos
+library(psych) # pacote para estatisticas descritivas
+library(dplyr) # pacote para organizacao e tratamento dos dados
+library(tidyr) # pacote para organizacao e tratamento dos dados
+library(DescTools) # pacote para correcao de outliers
+library(normtest) # pacote para testes de normalidade
+library(openxlsx) # leitura dos dados em excel
+library(readxl) # leitura dos dados em Excel
+library(stargazer) # tabelas das regressoes
+library(Hmisc) # correlacoes e testes
+library(tidyverse) # organizar os dados
 library(caret) # testes estatisticos
-library(car) #econometria
-library(lmtest) #testes lineares
-library (sandwich) #correcao de heterocedasticidade
-library(ISwR) #mudar os nomes
-library(MASS) #analise multivariada
-library(strucchange) #organizacao do banco de dados
+library(car) # econometria
+library(lmtest) # testes lineares
+library (sandwich) # correcao de heterocedasticidade
+library(ISwR) # mudar os nomes
+library(MASS) # analise multivariada
+library(strucchange) # organizacao do banco de dados
 
 
 # 2. Baixando os dados: 
-setwd("~/Mestrado/Igor_UFPR")
+setwd("~/Mestrado/dados/enad")
 
 microdados <- read.table("microdados_enade_2018.txt",header = TRUE, 
                                sep=";", dec = ",", 
@@ -34,23 +34,23 @@ microdados <- read.table("microdados_enade_2018.txt",header = TRUE,
 
 # 3.1 Variaveis selecionadas:
 
-# desempenho = NT_GER (Nota bruta da prova - Média ponderada da 
-# formação geral (25%) e componente específico (75%) (valor de 0 a 100))
+# desempenho = NT_GER (Nota bruta da prova - MÃ©dia ponderada da 
+# formaÃ§Ã£o geral (25%) e componente especÃ­fico (75%) (valor de 0 a 100))
 
-# esforco = QE_I23 (Quantas horas por semana, aproximadamente, você dedicou aos 
+# esforco = QE_I23 (Quantas horas por semana, aproximadamente, vocÃª dedicou aos 
 # estudos, excetuando as horas de aula?)
 
-# raça = QE_I02 (raça cor)
+# raÃ§a = QE_I02 (raÃ§a cor)
 
-# família = QE_107 (Quantas pessoas da sua família moram com você? Considere 
-# seus pais, irmãos, cônjuge, filhos e outros parentes que moram na 
-# mesma casa com você.)
+# famÃ­lia = QE_107 (Quantas pessoas da sua famÃ­lia moram com vocÃª? Considere 
+# seus pais, irmÃ£os, cÃ´njuge, filhos e outros parentes que moram na 
+# mesma casa com vocÃª.)
 
-# história = QE_I12 (Ao longo da sua trajetória acadêmica, você recebeu algum 
-# tipo de bolsa de permanência? No caso de haver mais de uma opção, 
-# marcar apenas a bolsa de maior duração.)
+# histÃ³ria = QE_I12 (Ao longo da sua trajetÃ³ria acadÃªmica, vocÃª recebeu algum 
+# tipo de bolsa de permanÃªncia? No caso de haver mais de uma opÃ§Ã£o, 
+# marcar apenas a bolsa de maior duraÃ§Ã£o.)
 
-# estrutura = QE_I08 (Qual a renda total de sua família, incluindo seus rendimentos?)
+# estrutura = QE_I08 (Qual a renda total de sua famÃ­lia, incluindo seus rendimentos?)
 
 # sexo = TP_SEXO
 
@@ -75,10 +75,10 @@ dados <- dados %>% rename("desempenho"="NT_GER", "esforco"="QE_I23",
 
 # 3.3. Ajustando os dados:
 
-# a) Criando uma variável multinomial para esforço:
+# a) Criando uma variÃ¡vel multinomial para esforÃ§o:
 
-# A = Nenhuma, apenas assisto às aulas.
-# B = De uma a três.
+# A = Nenhuma, apenas assisto Ã s aulas.
+# B = De uma a trÃªs.
 # C = De quatro a sete.
 # D = De oito a doze.
 # E = Mais de doze.
@@ -90,12 +90,12 @@ dados$esforco <- (ifelse(dados$esforco == "A", 0, 0) +
                          ifelse(dados$esforco == "E", 5, 0))
 
 
-# b) Criando uma variável multinomial para família:
+# b) Criando uma variÃ¡vel multinomial para famÃ­lia:
 
 # A = Nenhuma.
 # B = Uma.
 # C = Duas.
-# D = Três.
+# D = TrÃªs.
 # E = Quatro.
 # F = Cinco.
 # G = Seis.
@@ -111,27 +111,27 @@ dados$familia <- (ifelse(dados$familia == "A", 0, 0) +
                     ifelse(dados$familia == "G", 5, 0) +
                     ifelse(dados$familia == "H", 5, 0))
 
-# c) Criando uma variável binaria dicotomica para historia: 1 se recebeu auxilio
-# ou 0 caso contrário:
+# c) Criando uma variÃ¡vel binaria dicotomica para historia: 1 se recebeu auxilio
+# ou 0 caso contrÃ¡rio:
 
 # A = Nenhum.
-# B = Auxílio moradia.
-# C = Auxílio alimentação.
-# D = Auxílio moradia e alimentação.
-# E = Auxílio Permanência.
-# F = Outro tipo de auxílio.
+# B = AuxÃ­lio moradia.
+# C = AuxÃ­lio alimentaÃ§Ã£o.
+# D = AuxÃ­lio moradia e alimentaÃ§Ã£o.
+# E = AuxÃ­lio PermanÃªncia.
+# F = Outro tipo de auxÃ­lio.
 
 dados$historia <- ifelse(dados$historia == "A", 0, 1)
 
-# d) Criando uma variável multinomial para estrutura:
+# d) Criando uma variÃ¡vel multinomial para estrutura:
 
-# A = Até 1,5 salário mínimo (até R$ 1.431,00).
-# B = De 1,5 a 3 salários mínimos (R$ 1.431,01 a R$ 2.862,00).
-# C = De 3 a 4,5 salários mínimos (R$ 2.862,01 a R$ 4.293,00).
-# D = De 4,5 a 6 salários mínimos (R$ 4.293,01 a R$ 5.724,00).
-# E = De 6 a 10 salários mínimos (R$ 5.724,01 a R$ 9.540,00).
-# F = De 10 a 30 salários mínimos (R$ 9.540,01 a R$ 28.620,00).
-# G = Acima de 30 salários mínimos (mais de R$ 28.620,00).
+# A = AtÃ© 1,5 salÃ¡rio mÃ­nimo (atÃ© R$ 1.431,00).
+# B = De 1,5 a 3 salÃ¡rios mÃ­nimos (R$ 1.431,01 a R$ 2.862,00).
+# C = De 3 a 4,5 salÃ¡rios mÃ­nimos (R$ 2.862,01 a R$ 4.293,00).
+# D = De 4,5 a 6 salÃ¡rios mÃ­nimos (R$ 4.293,01 a R$ 5.724,00).
+# E = De 6 a 10 salÃ¡rios mÃ­nimos (R$ 5.724,01 a R$ 9.540,00).
+# F = De 10 a 30 salÃ¡rios mÃ­nimos (R$ 9.540,01 a R$ 28.620,00).
+# G = Acima de 30 salÃ¡rios mÃ­nimos (mais de R$ 28.620,00).
 
 
 dados$estrutura <- (ifelse(dados$estrutura == "A", 1, 0) + 
@@ -145,7 +145,7 @@ dados$estrutura <- (ifelse(dados$estrutura == "A", 1, 0) +
 # e) Criando varivel binaria para sexo: 1 para F (feminino) e 0 caso contrario:
 dados$sexo <- ifelse(dados$sexo == "F", 1, 0)
 
-# f) Criando varivel binaria para raça: 1 para A (branca) e 0 caso contrario:
+# f) Criando varivel binaria para raÃ§a: 1 para A (branca) e 0 caso contrario:
 dados$raca <- ifelse(dados$raca == "A", 1, 0)
 
 # g) Criando variavel binaria para a ufpr:
@@ -171,7 +171,7 @@ write.xlsx(dados, "dados2.xlsx")
 rm(microdados)
 dados <- read_excel("dados.xlsx")
 
-# 4.1. Fazendo as transformações nos dados:
+# 4.1. Fazendo as transformaÃ§Ãµes nos dados:
 dados$desempenho <- ifelse(dados$desempenho > 0, log(dados$desempenho), 0)
 
 # 5. Estatisticas descritivas das variaveis quantitativas nao binarias:
@@ -184,7 +184,7 @@ descritivas
 write.xlsx(descritivas, "descritivas.xlsx", row.names = T)
 
 
-# 7. Correlações lineares de spearman:
+# 7. CorrelaÃ§Ãµes lineares de spearman:
 correlates <- rcorr(as.matrix(dados),
                     type=c("spearman"))
 
@@ -192,7 +192,7 @@ correlacoes <- data.frame(round(correlates$r, 2))
 sig_corr <- data.frame(round(correlates$P))
 
 write.xlsx(correlacoes, "correlacoes.xlsx", row.names = T)
-# Só a correlação entre educ de mulheres e homens foi maior que 0,95.
+# SÃ³ a correlaÃ§Ã£o entre educ de mulheres e homens foi maior que 0,95.
 
 # Signficancia das correalacoes:
 correlacoes_p <- data.frame(correlates$P)
